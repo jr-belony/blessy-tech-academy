8584bde20c0f7c77baa6b7e7dad7497667380ce0
Jean Raymond BELONY <jeanbelony13@gmail.com>
2026-06-14 13:27:53 -0400

    Phase 4 Lecon 2 : templates Django, CSS statique, 4 pages

--- blessy_tech_academy/academie/admin.py
+++ blessy_tech_academy/academie/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Formation, Inscription
 
-# Register your models here.
+
+@admin.register(Formation)
+class FormationAdmin(admin.ModelAdmin):
+    list_display = ['nom', 'duree_mois', 'prix', 'actif']
+    list_filter = ['actif']
+    search_fields = ['nom', 'description']
+    list_editable = ['actif', 'prix']
+
+
+@admin.register(Inscription)
+class InscriptionAdmin(admin.ModelAdmin):
+    list_display = ['prenom', 'nom', 'email', 'formation', 'date_inscription', 'traite']
+    list_filter = ['traite', 'formation']
+    search_fields = ['prenom', 'nom', 'email']
+    list_editable = ['traite']
+    readonly_fields = ['date_inscription']
